065d2aecc14b90b349fdba6e222e936cf23a7279
Johannes Ekberg <uppfinnarn@gmail.com>
2013-01-15 15:49:11 +0100

    Initial Commit

---
+++ Core/MCKernel.cpp
@@ -0,0 +1,29 @@
+//
+//  MCKernel.cpp
+//  MacaroniCore
+//
+//  Created by Johannes Ekberg on 2013-01-14.
+//  Copyright (c) 2013 MacaroniCode. All rights reserved.
+//
+
+#include "MCKernel.h"
+
+MCKernel::MCKernel()
+{
+	this->startup();
+}
+
+MCKernel::~MCKernel()
+{
+	this->shutdown();
+}
+
+void MCKernel::startup()
+{
+	
+}
+
+void MCKernel::shutdown()
+{
+	
+}
---
+++ MC.h
@@ -0,0 +1,14 @@
+//
+//  MC.h
+//  MacaroniCore
+//
+//  Created by Johannes Ekberg on 2013-01-14.
+//  Copyright (c) 2013 MacaroniCode. All rights reserved.
+//
+
+#ifndef MacaroniCore_MC_h
+#define MacaroniCore_MC_h
+
+
+
+#endif
